742521909c94973c0c1390192218468e8444dc17
ThierryTheories <thierryrichie1919@gmail.com>
2026-09-14 22:52:20 +0700

    Creating the board

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ ROWS = 6
 COLUMNS = 7
 
 def create_board():
-    return [[" " for x in range(COLUMNS)] for x in range(ROWS)]
+    return [[" " for _ in range(COLUMNS)] for _ in range(ROWS)]
+
+def print_board(board):
+    
+    for row in board:
+        print("|" +  "|".join(row) + "|")
+    print("-" * (COLUMNS * 3 - 6))
+    print(" 1 2 3 4 5 6 7")
+    
+
+board = create_board()
+print_board(board)  
